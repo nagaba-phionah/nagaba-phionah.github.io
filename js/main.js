f7881efcf763c2683348bce7c93acc24608c8701
nagaba-phionah <pnagaba9@gmail.com>
2020-04-12 23:31:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//var estimater = require('./estimator');
+var estimater = require('./estimator');
 alert("Loading javascript ... ");
   
 document.addEventListener('load', function(){ alert("Js loaded");
