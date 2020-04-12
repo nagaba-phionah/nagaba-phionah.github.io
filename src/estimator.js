156181d8cc42c1076e56a12366f11fd255e1e7c9
nagaba-phionah <pnagaba9@gmail.com>
2020-04-12 23:46:07 +0300

    Changed the output object name from my_object to MyOutput

--- src/estimator.js
+++ src/estimator.js
@@ -14,28 +14,28 @@ const covid19ImpactEstimator = (data) => {
 		totalHospitalBeds: 1380614
 	}; */
 	
-	let my_output = {
+	let MyOutput = {
 		data: data,			// the input data I received
 		impact: {},			// my best case estimation
 		severeImpact: {}  // my severe case estimation 
 	}
-	const requestedTimeInDays = nomalizeTimeIntoDays(my_output.data.periodType, my_output.data.periodType.timeToElapse);
-	my_output.impact.currentlyInfected = my_output.data.reportedCases*10;
-	my_output.impact.infectionsByRequestedTime = my_output.impact.currentlyInfected*(2^(requestedTimeInDays/3));
-	my_output.impact.severeCasesByRequestedTime = 0.15*my_output.impact.infectionsByRequestedTime;
-	my_output.impact.hospitalBedsByRequestedTime = (0.35*my_output.data.totalHospitalBeds)-my_output.impact.severeCasesByRequestedTime;    
-	my_output.impact.casesForICUByRequestedTime = 0.05*my_output.impact.infectionsByRequestedTime;
-	my_output.impact.casesForVentilatorsByRequestedTime = 0.02*my_output.impact.infectionsByRequestedTime;
-	my_output.impact.dolarsInFlight = (my_output.data.region.avgDailyIncomePopulation*my_output.data.population)*(my_output.data.region.avgDailyIncomeInUSD*my_output.impact.infectionsByRequestedTime)*requestedTimeInDays;
+	const requestedTimeInDays = nomalizeTimeIntoDays(MyOutput.data.periodType, MyOutput.data.periodType.timeToElapse);
+	MyOutput.impact.currentlyInfected = MyOutput.data.reportedCases*10;
+	MyOutput.impact.infectionsByRequestedTime = MyOutput.impact.currentlyInfected*(2^(requestedTimeInDays/3));
+	MyOutput.impact.severeCasesByRequestedTime = 0.15*MyOutput.impact.infectionsByRequestedTime;
+	MyOutput.impact.hospitalBedsByRequestedTime = (0.35*MyOutput.data.totalHospitalBeds)-MyOutput.impact.severeCasesByRequestedTime;    
+	MyOutput.impact.casesForICUByRequestedTime = 0.05*MyOutput.impact.infectionsByRequestedTime;
+	MyOutput.impact.casesForVentilatorsByRequestedTime = 0.02*MyOutput.impact.infectionsByRequestedTime;
+	MyOutput.impact.dolarsInFlight = (MyOutput.data.region.avgDailyIncomePopulation*MyOutput.data.population)*(MyOutput.data.region.avgDailyIncomeInUSD*MyOutput.impact.infectionsByRequestedTime)*requestedTimeInDays;
 	//---------------------------
-	my_output.severeImpact.currentlyInfected = my_output.data.reportedCases*50;
-	my_output.severeImpact.infectionsByRequestedTime = my_output.severeImpact.currentlyInfected*(2^(requestedTimeInDays/3));         
-	my_output.severeImpact.severeCasesByRequestedTime = 0.15*my_output.severeImpact.infectionsByRequestedTime; 
-	my_output.severeImpact.hospitalBedsByRequestedTime = (0.35*my_output.data.totalHospitalBeds)-my_output.severeImpact.severeCasesByRequestedTime;    
-	my_output.severeImpact.casesForICUByRequestedTime = 0.05*my_output.severeImpact.infectionsByRequestedTime;
-	my_output.severeImpact.casesForVentilatorsByRequestedTime = 0.02*my_output.severeImpact.infectionsByRequestedTime;
-	my_output.severeImpact.dolarsInFlight = (my_output.data.region.avgDailyIncomePopulation*my_output.data.population)*(my_output.data.region.avgDailyIncomeInUSD*my_output.severeImpact.infectionsByRequestedTime)*requestedTimeInDays;      
-	return my_output;
+	MyOutput.severeImpact.currentlyInfected = MyOutput.data.reportedCases*50;
+	MyOutput.severeImpact.infectionsByRequestedTime = MyOutput.severeImpact.currentlyInfected*(2^(requestedTimeInDays/3));         
+	MyOutput.severeImpact.severeCasesByRequestedTime = 0.15*MyOutput.severeImpact.infectionsByRequestedTime; 
+	MyOutput.severeImpact.hospitalBedsByRequestedTime = (0.35*MyOutput.data.totalHospitalBeds)-MyOutput.severeImpact.severeCasesByRequestedTime;    
+	MyOutput.severeImpact.casesForICUByRequestedTime = 0.05*MyOutput.severeImpact.infectionsByRequestedTime;
+	MyOutput.severeImpact.casesForVentilatorsByRequestedTime = 0.02*MyOutput.severeImpact.infectionsByRequestedTime;
+	MyOutput.severeImpact.dolarsInFlight = (MyOutput.data.region.avgDailyIncomePopulation*MyOutput.data.population)*(MyOutput.data.region.avgDailyIncomeInUSD*MyOutput.severeImpact.infectionsByRequestedTime)*requestedTimeInDays;      
+	return MyOutput;
 };
 
 const nomalizeTimeIntoDays = (periodType, timeToElapse) => {
